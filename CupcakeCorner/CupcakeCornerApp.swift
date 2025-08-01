65dab47e641ac22b55b26a53138d5a117a1b656f
abayellow <sasha.abanshin@gmail.com>
2025-08-01 13:00:41 +1100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alexander Abanshin on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
